524a66865bddffb48f5e543804aecec60734f9ac
Jasmine <jasmineenams@gmail.com>
2023-06-21 07:57:34 -0700

    stack functions

---
+++ monty0.c
@@ -0,0 +1,94 @@
+#include "monty.h"
+int value;
+/**
+ * new_Node - function that creates a new node
+ * @n: input
+ * Return: new node
+ */
+stack_t *new_Node(int n)
+{
+	stack_t *new = NULL;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	return (new);
+}
+
+/**
+ * _push - function that pushes an item
+ * @stack: parameter
+ * @line_number: input
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new = NULL;
+	(void)line_number;
+
+	new = new_Node(value);
+
+	new->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new;
+	*stack = new;
+}
+
+/**
+ * _pall - function that prints elements of a stack
+ * @stack: parameter
+ * @n: value
+ */
+void _pall(stack_t **stack, unsigned int n)
+{
+	stack_t *current = NULL;
+	(void)n;
+
+	current = *stack;
+
+	while (current != NULL)
+	{
+		dprintf(STDOUT_FILENO, "%d\n", current->n);
+		current = current->next;
+	}
+}
+
+/**
+ * free_dlistint - function that frees a list.
+ * @stack: parameter
+ */
+void free_dlistint(stack_t *stack)
+{
+	stack_t *current = NULL;
+
+	current = stack;
+
+	if (current != NULL)
+	{
+		free_dlistint(current->next);
+		free(current);
+	}
+}
+
+/**
+ * _pint - function that prints the value at the top of the stack.
+ * @stack: parameter
+ * @line_number: Number
+ */
+void _pint(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !stack)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
+		cleanStack(stack);
+		exit(EXIT_FAILURE);
+	}
+	else
+		dprintf(STDOUT_FILENO, "%d\n", (*stack)->n);
+}
